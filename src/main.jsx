d01d363b5d98afa535a7c9793cd569c6f0029455
SwornaCode <kasfiamostafa03@gmail.com>
2023-12-14 10:45:50 +0600

    global css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./Router/router.jsx";
+// import { RouterProvider } from "react-router-dom";
+// import router from "./Router/router.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App></App>
   </React.StrictMode>
 );
